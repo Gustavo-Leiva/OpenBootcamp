4c0b9b36d4b178c276b3e68ec02cb60557829c6c
Gustavo Leiva <leiva.gustavo@hotmail.com.ar>
2022-08-04 17:13:38 -0300

    agregar al proyecto

---
+++ Clases/Javascript/Sesion-01-02-mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,13 @@
+//Este es el mensaje de bienvenida
+
+/*
+Esta es la parte intermedia del proyecto del
+En esat parte realizamos los chqueos necesiarios 
+Por ultimo vamos a la ultima linea de saludo
+Esto tambien está comentados
+Aquí también.
+*/
+
+
+//Este va a ser el mensaje que se envia para despedir
+console.log("Adiós");
