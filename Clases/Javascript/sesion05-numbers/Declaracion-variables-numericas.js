c06767c361d0fbb30fffc45d153b14730230bda1
Gustavo Leiva <leiva.gustavo@hotmail.com.ar>
2022-08-09 14:01:41 -0300

    agregar al proyecto

---
+++ Clases/Javascript/sesion05-numbers/Declaracion-variables-numericas.js
@@ -0,0 +1,16 @@
+//Sesion 05 -numbers
+//Declaracion de variables numericas enteras y con decimales
+let a = 5;
+console.log(a);
+
+let b = 0.1;
+console.log(b);
+
+//Precisión
+
+let c = 0.2;
+
+console.log(b + c);
+
+//pequeño truco para obtener valores "reales"
+console.log(Math.round((0.1 + 0.2)*100)/100);
